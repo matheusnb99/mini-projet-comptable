d2e83b5f6a17f981ffe8a5619bba4afddfa745d4
matheusnb99 <matheusintelectual123@hotmail.com>
2022-01-09 23:50:19 +0100

    visual changes

--- pages/components/ui/FormField.jsx
+++ pages/components/ui/FormField.jsx
@@ -8,9 +8,9 @@ const FormField = (props) => {
     <Field {...otherProps}>
       {({ field, meta: { touched, error } }) => (
         <div className={className}>
-          <label className="block">
+          <label className="block w-2/3 flex items-center justify-between">
             {children}
-            <Component className="w-full" {...field} {...otherProps} />
+            <Component className="w-2/3 px-5 mx-5" {...field} {...otherProps} />
           </label>
           {touched && error ? (
             <p className="text-red-500 p-2 text-sm">{error}</p>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
-import styles from "../styles/Home.module.css"
 import TableauComptable from "./components/TableauComptable"
 import Link from "next/link"
 const Home = () => {
   return (
-    <div className={styles.container}>
+    <div className="flex justify-between flex-wrap">
+      <div className="basis-[100%]">
+        <h1>DASHBOARD</h1>
+        <Link href="/form-page">
+          <a>Add new entry</a>
+        </Link>
+      </div>
       <TableauComptable></TableauComptable>
-      <Link href="/form-page">
-        <a>Add new entry</a>
-      </Link>
     </div>
   )
 }
