7c3879a3a401faad62c5697f98dcde6529e64f2a
matheusnb99 <matheusintelectual123@hotmail.com>
2022-01-14 15:09:35 +0100

    centered index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import TableauComptable from "../src/components/TableauComptable"
 import Link from "next/link"
 const Home = () => {
   return (
-    <div className="flex flex-col  flex-wrap">
+    <div className="flex flex-col flex-wrap items-center">
       <div className="basis-[100%]">
         <h1 className="text-2xl">DASHBOARD</h1>
         <Link href="/form-page">
